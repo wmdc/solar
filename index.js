d2a8db4fecd07604fbb61ff40d1484e4849f6d71
Michael Welsman-Dinelle <mwelsmandinelle@gmail.com>
2016-12-27 14:09:04 -0800

    Fix perspective aspect ratio

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var scene = new THREE.Scene();
 var aspect = window.innerWidth / window.innerHeight;
 
 var camera = new THREE.OrthographicCamera(
-	-2 * aspect,
-    2 * aspect,
-    aspect,
 	-aspect,
+    aspect,
+    1,
+	-1,
     0,
     100
 );
@@ -19,34 +19,36 @@ var texture = new THREE.TextureLoader().load( "earth.jpg" );
 texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
 
-var geometry = new THREE.SphereGeometry(1, 128, 128);
+var geometry = new THREE.SphereGeometry(0.8, 128, 128);
 var material = new THREE.MeshPhongMaterial({ map: texture });
-// var lineMaterial = new THREE.MeshBasicMaterial({
-//     wireframe: true,
-//     color: 0x000000
-// });
-var cube = new THREE.Mesh( geometry, material );
-//var lineCube = new THREE.Mesh( geometry, lineMaterial );
-scene.add( cube );
-//scene.add( lineCube );
+var lineMaterial = new THREE.MeshBasicMaterial({
+     wireframe: true,
+     color: 0x222222
+});
 
-var ambLight = new THREE.AmbientLight(0x202020);
+var cube = new THREE.Mesh( geometry, material );
+var lineCube = new THREE.Mesh( geometry, lineMaterial );
+scene.add( cube );
+scene.add( lineCube );
+
+var ambLight = new THREE.AmbientLight(0x404040);
 scene.add(ambLight);
 
 var light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(-1, 0, 0);
 scene.add(light);
 
-camera.position.z = 2;
+// doesn't matter as much for orthographic, as long as the scene is inside the clipping planes
+camera.position.z = 100;
 
 var render = function () {
     requestAnimationFrame( render );
 
-//    cube.rotation.x += 0.01;
+//     cube.rotation.x += 0.01;
     cube.rotation.y += 0.001;
 
 //    lineCube.rotation.x += 0.01;
-//    lineCube.rotation.y += 0.001;
+    lineCube.rotation.y += 0.001;
 
     renderer.render(scene, camera);
 };
