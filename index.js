ee6b20f6dad2a4965b9ca6992a4626551632ae6d
Michael Welsman-Dinelle <mwelsmandinelle@gmail.com>
2016-12-27 12:23:48 -0800

    Use orthographic projection so the poles are visible

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth/window.innerHeight,
-    0.1,
-    1000
+
+var aspect = window.innerWidth / window.innerHeight;
+
+var camera = new THREE.OrthographicCamera(
+	-2 * aspect,
+    2 * aspect,
+    aspect,
+	-aspect,
+    0,
+    100
 );
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -14,22 +19,22 @@ var texture = new THREE.TextureLoader().load( "earth.jpg" );
 texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
 
-var geometry = new THREE.SphereGeometry(1, 32, 32);
+var geometry = new THREE.SphereGeometry(1, 128, 128);
 var material = new THREE.MeshPhongMaterial({ map: texture });
-var lineMaterial = new THREE.MeshBasicMaterial({
-    wireframe: true,
-    color: 0x000000
-});
+// var lineMaterial = new THREE.MeshBasicMaterial({
+//     wireframe: true,
+//     color: 0x000000
+// });
 var cube = new THREE.Mesh( geometry, material );
-var lineCube = new THREE.Mesh( geometry, lineMaterial );
+//var lineCube = new THREE.Mesh( geometry, lineMaterial );
 scene.add( cube );
-scene.add( lineCube );
+//scene.add( lineCube );
 
 var ambLight = new THREE.AmbientLight(0x202020);
 scene.add(ambLight);
 
 var light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(100, 0, 50);
+light.position.set(-1, 0, 0);
 scene.add(light);
 
 camera.position.z = 2;
@@ -37,11 +42,11 @@ camera.position.z = 2;
 var render = function () {
     requestAnimationFrame( render );
 
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+//    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.001;
 
-    // lineCube.rotation.x += 0.01;
-    // lineCube.rotation.y += 0.01;
+//    lineCube.rotation.x += 0.01;
+//    lineCube.rotation.y += 0.001;
 
     renderer.render(scene, camera);
 };
