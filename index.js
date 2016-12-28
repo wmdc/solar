987b523a6b424ebb25016923d2c33d360fec31a7
Michael Welsman-Dinelle <mwelsmandinelle@gmail.com>
2016-12-27 20:34:14 -0800

    Clean up code and fix moon texture

--- index.js
+++ index.js
@@ -11,47 +11,40 @@ var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var texture = new THREE.TextureLoader().load( "earth.jpg" );
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
+var textures = {
+    earth: new THREE.TextureLoader().load( "earth.jpg" ),
+    moon: new THREE.TextureLoader().load("moon.jpg"),
+    sky: new THREE.TextureLoader().load("sky.jpg"),
+    sun: new THREE.TextureLoader().load("sun.jpg")
+};
 
-var skyTexture = new THREE.TextureLoader().load("sky.jpg");
-skyTexture.wrapS = THREE.RepeatWrapping;
-skyTexture.wrapT = THREE.RepeatWrapping;
-
-var moonTexture = new THREE.TextureLoader().load("moon.jpg");
-moonTexture.wrapS = THREE.RepeatWrapping;
-moonTexture.wrapT = THREE.RepeatWrapping;
+var materials = {
+    earth: new THREE.MeshPhongMaterial({ map: textures.earth }),
+    moon: new THREE.MeshPhongMaterial({ map: textures.moon }),
+    sky: new THREE.MeshBasicMaterial({ map: textures.sky, side: THREE.BackSide }),
+    sun: undefined
+};
 
 var moonGeometry = new THREE.SphereGeometry(0.6, 16, 16);
-var moonMaterial = new THREE.MeshBasicMaterial({ map: skyTexture });
-var moon = new THREE.Mesh(moonGeometry, moonMaterial);
-moon.position.x = 15;
+var moon = new THREE.Mesh(moonGeometry, materials.moon);
+moon.position.x = 10;
 
 var moonPivot = new THREE.Object3D();
 
 var skyGeometry = new THREE.SphereGeometry(1000, 8, 8);
-var skyMaterial = new THREE.MeshBasicMaterial({ map: skyTexture, side: THREE.BackSide });
-var sky = new THREE.Mesh(skyGeometry, skyMaterial);
+var sky = new THREE.Mesh(skyGeometry, materials.sky);
 sky.position.z = 0;
 
 scene.add(sky);
 
 var geometry = new THREE.SphereGeometry(3, 32, 32);
-var material = new THREE.MeshPhongMaterial({ map: texture });
-var lineMaterial = new THREE.MeshBasicMaterial({
-     wireframe: true,
-     color: 0x222222
-});
 
-var cube = new THREE.Mesh( geometry, material );
-var lineCube = new THREE.Mesh( geometry, lineMaterial );
-cube.position.z += 80;
-scene.add(cube);
+var earth = new THREE.Mesh( geometry, materials.earth);
+earth.position.z += 80;
+scene.add(earth);
 
 moonPivot.add(moon);
-cube.add(moonPivot);
-// scene.add( lineCube );
+earth.add(moonPivot);
 
 var axis = new THREE.AxisHelper(10);
 scene.add(axis);
@@ -70,9 +63,8 @@ var render = function () {
     requestAnimationFrame( render );
 
     sky.rotation.y += 0.0001;
-    cube.rotation.y += 0.01;
+    earth.rotation.y += 0.01;
     moonPivot.rotation.y += 0.01;
-    lineCube.rotation.y += 0.001;
 
     renderer.render(scene, camera);
 };
